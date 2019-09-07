be16450db604b6573504ea9b3665e2289aab25a6
name <email>
2019-09-07 17:49:38 +0800

    uodate

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
   <div id="app">
-    hellow world
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
-<script type="text/ecmascript-6">
-
+<script>
+export default {
+  name: 'App'
+}
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
-@import "~common/stylus/variable"
-
-#app
- color:$color-theme
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,26 +1,15 @@
-import 'babel-polyfill'
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastclick from 'fastclick'
-import VueLazyload from 'vue-lazyload'
-import store from './store'
 
-import 'common/stylus/index.styl'
-
-/* eslint-disable no-unused-vars */
-// import vConsole from 'vconsole'
-
-fastclick.attach(document.body)
-
-Vue.use(VueLazyload, {
-  loading: require('common/image/default.png')
-})
+Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  store,
-  render: h => h(App)
+  components: { App },
+  template: '<App/>'
 })
