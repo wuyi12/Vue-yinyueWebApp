0544efb5f716f66288e0f44c73d1568a6aebc281
name <email>
2019-09-07 17:40:47 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,16 @@
 <template>
-  <div id="app" @touchmove.prevent>
-    <m-header></m-header>
-    <tab></tab>
-    <keep-alive>
-      <router-view></router-view>
-    </keep-alive>
-    <player></player>
+  <div id="app">
+    hellow world
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import MHeader from 'components/m-header/m-header'
-  import Player from 'components/player/player'
-  import Tab from 'components/tab/tab'
 
-  export default {
-    components: {
-      MHeader,
-      Tab,
-      Player
-    }
-  }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+@import "~common/stylus/variable"
+
+#app
+ color:$color-theme
 </style>
